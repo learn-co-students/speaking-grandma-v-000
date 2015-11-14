01c83c997f79cd7f2451db32532d1835be2dc732
matpa <mateipopa@icloud.com>
2015-11-13 17:58:05 -0800

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,16 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(interjection)
+  check_upcase_form=interjection.upcase
+  if check_upcase_form==interjection
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+
+
+
+    # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
