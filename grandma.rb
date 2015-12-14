bc4408aa17d1def6c4b7bdb299ec25d3cd02e801
jamcelr <jamcelr@hotmail.com>
2015-12-14 09:57:37 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,12 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.downcase || phrase.capitalize
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
