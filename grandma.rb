95bed61a633ce9723b043ce252ddd9fec4f5e5a0
jaeku <ja.ku@rutgers.edu>
2016-01-06 06:01:11 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,15 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
