6da34ed6cff367317af93c43a01753cb8f072db5
mackdalton <mackdalton@gmail.com>
2016-01-05 20:39:14 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,17 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+phrase = "HELLO GRANDMA"
+
+def speak_to_grandma(phrase)
+  if (phrase != phrase.upcase)
+    return "HUH?! SPEAK UP, SONNY!"
+
+  else 
+      return "NO, NOT SINCE 1938!"
+    end
+  
+  end
+
+speak_to_grandma(phrase)
