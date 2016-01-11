a1055c8ce414754f54d5f2efc4503401d837016e
jenjiyi <jenny.yamada@gmail.com>
2016-01-11 11:18:34 -0800

    if/else lab done

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,14 @@
 # Write a speak_to_grandma method.
-
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
+def speak_to_grandma (phrase)
+  # Whatever you say to grandma, she should respond with
+  if phrase == phrase.upcase
+      # If you shout, she can hear you (or at least she thinks so) 
+      # and yells back
+      # NO, NOT SINCE 1938!
+     "NO, NOT SINCE 1938!"
+  else
+      # HUH?! SPEAK UP, SONNY!
+      # unless you shout it (type in all capitals).
+      "HUH?! SPEAK UP, SONNY!"
+  end 
+end
