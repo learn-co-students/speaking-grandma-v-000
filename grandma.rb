07609c26e084d9e5bd2d81f3271d5df8e0136887
lcorr8 <l.corr.v@gmail.com>
2016-01-16 10:09:31 -0500

    complete lab

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,12 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(hello)
+
+  if hello != hello.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+     return "NO, NOT SINCE 1938!"
+  end 
+end
