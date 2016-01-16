8161fe420eeed9912bd3927c36aaf16b315365b1
lawrend <douglaslawrence314@gmail.com>
2016-01-15 18:45:33 -0800

    done

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,13 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+require 'pry'
+
+def speak_to_grandma(message)
+  if message == message.upcase
+    "NO, NOT SINCE 1938!" 
+  else
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
