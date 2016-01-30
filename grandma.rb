80c1199f03cfd547ac0cc910039e41a7c5cd930f
mkrisch <mike.krisch@gmail.com>
2016-01-30 07:34:07 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,11 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(chat)
+  if "#{chat}" != "#{chat}".upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
