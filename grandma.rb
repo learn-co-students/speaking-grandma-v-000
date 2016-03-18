fd3d5148a96563e584fb7bffc220c491ef854cff
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-03-18 14:23:10 -0700

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,10 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+def speak_to_grandma(phrase)
+	if phrase.upcase != phrase
+		return "HUH?! SPEAK UP, SONNY!"
+	else 
+		return "NO, NOT SINCE 1938!"
+	end
+end
