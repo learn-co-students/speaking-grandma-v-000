9fd25f12d54b7a655fa132305ac414555c376891
mmelmer <mikemelmer@gmail.com>
2016-04-09 12:37:16 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,11 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(input)
+  if input == input.upcase
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
