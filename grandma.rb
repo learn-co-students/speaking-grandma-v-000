8f3bced8a8119764614a4643921d299f8af53cf8
vandepool <contact.vandepool@gmail.com>
2016-04-09 04:00:10 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,11 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(words)
+  if words == words.upcase
+    return "NO, NOT SINCE 1938!"
+    else
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+end
