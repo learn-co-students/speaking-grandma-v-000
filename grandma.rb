e0daddf2bf52115cd8880154575bec3bbe6c55cc
Nicholas-Maxwell <nick.maxwell18@gmail.com>
2016-04-14 15:24:26 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,15 @@
+ def speak_to_grandma(shout)
+
+  if shout.upcase == shout
+
+    "NO, NOT SINCE 1938!"
+
+  else 
+
+   "HUH?! SPEAK UP, SONNY!"
+  end 
+end 
+
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
@@ -8,3 +20,11 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+ 
+#if dog == "hungry"
+  #puts "Refilling food bowl."
+#else
+  #puts "Reading newspaper."
+#end
+ 
