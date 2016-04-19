5b69f787118ad6955e1f874914f29eb60583be50
dblem <dax.blem@gmail.com>
2016-04-18 20:44:49 -0400

    finished

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,16 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(speech)
+  if speech.to_s == speech.upcase
+    reply = "NO, NOT SINCE 1938!"
+  else
+    reply = "HUH?! SPEAK UP, SONNY!"
+  end
+  reply
+end
