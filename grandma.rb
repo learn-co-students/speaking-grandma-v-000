7fad69757a5e66603fef7887f61aa280b30fddfb
joelbitar1986 <joelbitar1986@gmail.com>
2016-04-25 20:21:18 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -8,3 +8,12 @@
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(argument)
+	
+	if argument == argument.upcase
+		return "NO, NOT SINCE 1938!"
+	else
+		return "HUH?! SPEAK UP, SONNY!"
+	end
+end
