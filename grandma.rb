9d4d6551f68d188b40d5effea603bd5c2731bc01
Ivan Pilot <ivanpilot@gmail.com>
2016-04-27 20:50:53 +0100

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,15 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
+
+def speak_to_grandma(phrase)
+  if phrase != phrase.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
+end
