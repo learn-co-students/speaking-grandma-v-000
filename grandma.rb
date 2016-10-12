970b3414b30353b9a7ae64aefe9ba5581612a9cb
Mary Umoh <umohm12@gmail.com>
2016-10-12 18:23:24 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -7,3 +7,23 @@ else phrase
   return "NO, NOT SINCE 1938!"
  end
 end
+
+
+# What I had w/ only 2 tests not passing
+#def speak_to_grandma(phrase)
+#if phrase == "I LOVE YOU GRANDMA!"
+  #return "I LOVE YOU TOO PUMPKIN!"
+#elsif  phrase
+  #return "HUH?! SPEAK UP, SONNY!"
+#elsif phrase  == "I LOVE YOU GRANDMA!"
+  #return "HUH?! SPEAK UP, SONNY!"
+#elsif phrase != phrase.upcase()
+  #return "NO, NOT SINCE 1938!"
+#elsif phrase != phrase.upcase()
+  #return "NO, NOT SINCE 1938!"
+#elsif phrase
+  #return "HUH?! SPEAK UP, SONNY!"
+#elsif phrase
+#  return 'I LOVE YOU TOO PUMPKIN!'
+#end
+#end
