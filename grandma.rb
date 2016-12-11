b7e2f1c0af42ce1dc7d2cc6d1893dc0327cea522
ryanpmcmahon <ryan@mcmahon.work>
2016-12-11 15:29:39 -0500

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,21 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(say_something)
+  if say_something != say_something.upcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif say_something == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
+end
