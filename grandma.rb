118ae56e022e4edab4cb80224e89cce95dcca2da
Chidinma Eze <ceze1990@gmail.com>
2017-06-04 00:48:53 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
-  if (phrase=="I LOVE YOU GRANDMA!")
+def speak_to_grandma(higrandma)
+  if (higrandma=="I LOVE YOU GRANDMA!")
     return "I LOVE YOU TOO PUMPKIN!"
-   elsif (phrase=="#{phrase}".upcase)
+  elsif (higrandma=="#{higrandma}".upcase)
     return "NO, NOT SINCE 1938!"
     else
     return "HUH?! SPEAK UP, SONNY!"
