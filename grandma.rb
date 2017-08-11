edd67e3438d7e821a413cb806e009b74a5a6c535
brandon wirz <brandonwirz2@brandons-MacBook-Pro.local>
2017-08-11 17:33:12 -0600

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
