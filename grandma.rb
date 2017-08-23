731f73401f0c68af936c9862e2450a9e29844a75
Janusz Szubert <jszubert87@gmail.com>
2017-08-23 22:40:34 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,10 @@
 
 def speak_to_grandma(phrase)
   if phrase === "I LOVE YOU GRANDMA!"
-      "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif phrase === phrase.upcase
+  elsif phrase.upcase
     "NO, NOT SINCE 1938!"
   end
 end
