62b89526d44be2cbeb7b9213df5ab1987b8e514f
Hannah Danziger <hdanziger8@gmail.com>
2017-09-19 16:52:37 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,10 +11,12 @@
 
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.upcase()
-    return "HUH?! SPEAK UP, SONNY!"
-  else
-    return "NO, NOT SINCE 1938!"
+if phrase == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+elsif phrase != phrase.upcase()
+  return "HUH?! SPEAK UP, SONNY!"
+elsif phrase = phrase.upcase()
+  return "NO, NOT SINCE 1938!"
   end
 end
 
