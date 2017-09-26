6b5dc857baab35690e5e21cfb87b5b1f347f3866
Jennifer Wilson <jennwilson55@gmail.com>
2017-09-26 23:36:12 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
-  else phrase != phrase.upcase
+  else
     "HUH?! SPEAK UP, SONNY!"
   end
 end
