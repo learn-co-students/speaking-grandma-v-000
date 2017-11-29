c161795370c0a74199b9138a506dd9bdc3f6c836
Eric Lin <esotericlin@gmail.com>
2017-11-29 01:08:48 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.reverse
+  elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
