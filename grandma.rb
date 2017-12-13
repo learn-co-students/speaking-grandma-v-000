838e36cef5f19d78a778ca8b2b210c8398cd6eb5
Alpha Daniel <daniel.nunez.nyc@gmail.com>
2017-12-13 04:10:41 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(str)
   if str == 'I LOVE YOU GRANDMA!'
-    puts 'I LOVE YOU TOO PUMPKIN!'
+    'I LOVE YOU TOO PUMPKIN!'
   elsif str == str.upcase  
-    puts 'NO, NOT SINCE 1938!'
+    'NO, NOT SINCE 1938!'
   else 
-    puts 'HUH?! SPEAK UP, SONNY!'
+    'HUH?! SPEAK UP, SONNY!'
   end
 end
