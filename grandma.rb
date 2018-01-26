adffa5f6b852f6967cb010acfddb227c5fc9f233
CharlaCeansky <github email address>
2018-01-26 11:24:05 -0500

    talking to grandma with control flow elsif

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,23 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma(phrase)
+    if phrase != phrase.upcase()
+        "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "I LOVE YOU GRANDMA!"
+        "I LOVE YOU TOO PUMPKIN!"
+    else 
+        "NO, NOT SINCE 1938!"
+    end
+end
+    
+#Write a speak_to_grandma method.
+#The method should take in a string argument containing a phrase and 
+#check to see if the phrase is written in all uppercase: if it isn't, then grandma can't hear you. 
+#She should then respond with (return) HUH?! SPEAK UP, SONNY!.
+#If you shout at her (i.e. call the method with a string argument containing a phrase that is all uppercase,
+#then she can hear you (or at least she thinks that she can) and should respond with 
+#(return) NO, NOT SINCE 1938!.
+#However, if the phrase is exactly equal to: "I LOVE YOU GRANDMA!", 
+#then Grandma is very pleased and definitely hears that! and should repond with 
+#(return) I LOVE YOU TOO PUMPKIN!
