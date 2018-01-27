05cd7c98bc2ef012a8032e8c4ba717ee689db14c
Kalil Salkey <ksalkey@gmail.com>
2018-01-27 19:56:14 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,19 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+greeting = "Hi grandma!"
+if greeting == "did you go run today?" .upcase
+  puts "NO, NOT SINCE 1938!"
+elsif greeting == "i love you grandma!".upcase
+  puts "i love you too!".upcase
+else
+  puts "huh?! speak up sonny!".upcase
+end
