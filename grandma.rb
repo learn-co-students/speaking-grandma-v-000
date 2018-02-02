1113be3917ccfdadf6eeac27ed170c5ea8a4a59f
Stephen Harrington <stephenharrington04@yahoo.com>
2018-02-02 04:26:15 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif statement == statement.upcase
-    "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else
-    "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end 
 end
