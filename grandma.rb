2424311cddaeaa89b51a5237c2747b710251d75c
Dale Terrell <tragicid@gmail.com>
2018-05-02 14:30:25 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase)
 if phrase == phrase.downcase
   return "HUH? SPEAK UP, SONNY!"
 elsif phrase == "I LOVE YOU GRANDMA!"
-  return "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"n
 else phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
   end
