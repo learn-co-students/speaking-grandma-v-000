a23cb42f5da1447158444f8dd51d7b9a206aea61
Dale Terrell <tragicid@gmail.com>
2018-05-02 14:14:40 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
-  puts "HUH?! SPEAK UP, SONNY!"
-if phrase == "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == upcase
-  puts "NO, NOT SINCE 1938!'
+if phrase == phrase.downcase
+  return "HUH? SPEAK UP, SONNY!"
+elsif phrase == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+else phrase == phrase.upcase
+  return "NO, NOT SINCE 1938!"
   end
-end
 end
