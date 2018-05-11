6f9205210d1b0bdbd8b1625a71e1756853ec6ad5
Adam Barton <adam.barton333@gmail.com>
2018-05-11 21:57:18 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,20 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(argument)
+    require 'pry'
+#    binding.pry
+    if argument == "I LOVE YOU GRANDMA!"
+      return "I LOVE YOU TOO PUMPKIN!"
+    end
+    if argument == argument.upcase()
+      return "NO, NOT SINCE 1938!"
+    else
+      return "HUH?! SPEAK UP, SONNY!"
+    end
+    
+end
+   
+  
+
