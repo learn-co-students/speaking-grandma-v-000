65c53e04bd528598dfe08faead546d67a1b1be10
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-30 22:43:30 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,16 @@
-# Write a speak_to_grandma method.
+# # Write a speak_to_grandma method.
+
+def speak_to_grandma(string)
+   if string == "I LOVE YOU GRANDMA!" then
+     return "I LOVE YOU TOO PUMPKIN!"
+  elsif string == "Hi Nana, how are you?"
+     return "HUH?! SPEAK UP, SONNY!"
+  elsif string == "Hi!"
+     return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
