f74d9ce4075f917a66d2b942deda0740159986e0
Bailey Sullivan <bailey.rose.sullivan@gmail.com>
2018-07-03 20:59:50 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,12 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == (phrase).downcase
-    puts "HUH?! SPEAK UP, SONNY!" 
-  else 
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  end 
-end 
+    "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
+  end
+end
