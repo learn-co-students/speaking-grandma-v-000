091f617d89315a7ef806b69cce47f1cbe5bdab04
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-12 03:53:12 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -16,6 +16,6 @@ def speak_to_grandma (phrase)
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  else phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMKIN!"
 end
