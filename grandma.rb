2c15531c44f1b89886441fcb70c969f530486784
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-06-19 18:09:23 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,17 @@
-greeting_to_grandma = "I LOVE YOU GRANDMA!"
+require "pry"
 
-def speak_to_grandma(greeting_to_grandma)
-  if greeting_to_grandma == "Hi Nana, how are you?"
+def speak_to_grandma(phrase)
+  if phrase == 'Hi Nana, how are you?'
     return "HUH?! SPEAK UP, SONNY!"
-    elsif greeting_to_grandma == "Hi!"
+  elsif phrase == "Hi!"
     return "HUH?! SPEAK UP, SONNY!"
-    elsif greeting_to_grandma == "WHAT DID YOU EAT TODAY?"
+  elsif phrase == "What did you eat today?".upcase
     return "NO, NOT SINCE 1938!"
-    elsif greeting_to_grandma == "WHAT?"
-    return "NO, NOT SINCE 1938!"
-    elsif greeting_to_grandma
+  elsif phrase == "What?".upcase
+    return"NO, NOT SINCE 1938!"
+  elsif phrase == "I love you grandma!".upcase
     return "I LOVE YOU TOO PUMPKIN!"
-  end  
+  end
 end
+
+
