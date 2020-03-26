65905389b54df9fac6f76e4dbc46a9606f68a777
vonvo-max <max.ringelheim@gmail.com>
2020-03-26 12:35:20 -0400

    Done.

--- grandma.rb
+++ grandma.rb
@@ -21,17 +21,44 @@
 #   end
 # end
 
+#correct code below
+#
+#def speak_to_grandma (phrase)
+#   if phrase == "I LOVE YOU GRANDMA!"
+#     return "I LOVE YOU TOO PUMPKIN!"
+#   elsif phrase == "Hi Nana, how are you?"
+#     return "HUH?! SPEAK UP, SONNY!"
+#   elsif phrase == "Hi!"
+#     return "HUH?! SPEAK UP, SONNY!"
+#   else phrase == "WHAT DID YOU EAT TODAY?"
+#   return "NO, NOT SINCE 1938!"
+#   end
+#end
+# correct code above
+
+#refactored code 1 below#
+# def speak_to_grandma (phrase)
+#   if phrase == "I LOVE YOU GRANDMA!"
+#     return "I LOVE YOU TOO PUMPKIN!"
+#   elsif phrase != phrase.upcase()
+#     return "HUH?! SPEAK UP, SONNY!"
+#   else phrase != phrase.downcase()
+#     return "NO, NOT SINCE 1938!"
+#   end
+# end
+#refactored code 1 above#
+
+#refactored code 2 below#
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == "Hi Nana, how are you?"
+  elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "Hi!"
-    return "HUH?! SPEAK UP, SONNY!"
-  else phrase == "WHAT DID YOU EAT TODAY?"
-  return "NO, NOT SINCE 1938!"
+  else
+    return "NO, NOT SINCE 1938!"
   end
 end
+#refactored code 2 above#
 
 
 
